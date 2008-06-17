03ac7697162039b9d841800e88e3bf12e3ff7749
Steren <steren@creativecommons.org>
2008-06-17 16:28:56 -0700

    Semantic Tasks new version working

--- ST_Notify_Assignment.php
+++ ST_Notify_Assignment.php
@@ -1,103 +1,145 @@
 <?php
-
-require_once($stIP . '/XPM4/SMTP.php'); // XPM4
-
-# This is a (fairly) simple hack to notify people when a new chapter page has been created.
-#   When a wiki article is saved,
-#   If that article is now a member of "Category:Chapter" AND it was not previously,
-#   Send an email to freedom@freeculture.org and the email address of the user that created/modified the article.
-
-# This is by no means an elegant hack. At the moment we use our own parser to determine category membership, as I
-# couldn't find another simple method to do this in a reasonable amount of time or searching. Hopefully
-# this will change in the near future.
-
-# (C) 2007 AMP and Asheesh Laroia
+# (C) 2008 Steren Giannini
 # Licensed under the GNU GPLv2 (or later).
 
-# CONFIGURATION
-$wpcSearchStrings = array("[[Category:Chapter]]", "{{Chapter");
-$wpcBotEmail = 'webmaster+tasks@creativecommons.org';
-$wpcEmailSubject = "";
-$wpcEmailMessage = "";
-$wpcAPI_Base = 'http://wiki.freeculture.org/api.php'; # FIXME could be detected
-
-# ------------------- CUT HERE --------------------
-# ONLY EDIT BELOW THIS LINE IF YOU THINK YOU ARE
-# SMART ENOUGH TO HACK BAD PHP
-#
-# DO NOT EDIT BELOW THIS LINE IF YOU REALIZE YOU ARE
-# TOO SMART TO HACK BAD PHP
-
-function st_get_assignees_to_notify(&$article) {
-
-        global $smwgIP;        
-	include_once($smwgIP . "/includes/SMW_QueryProcessor.php");
-        $events = array();
-        $query_string = "[[$date_property::*]][[$date_property::+]]$filter_query";
-        $params = array();
-        $inline = true;
-        $format = 'auto';
-        $printlabel = "";        
-	$printouts[] = new SMWPrintRequest(SMW_PRINT_THIS, $printlabel);
-
-	$query  = SMWQueryProcessor::createQuery($query_string, $params, $inline, $format, $printouts);        
-	$results = smwfGetStore()->getQueryResult($query);
-
-	while ($row = $results->getNext()) {
-		$event_names = $row[0];
-		$event_dates = $row[1];
-		$event_title = $event_names->getNextObject()->getTitle();
-	
-		while ($event_date = $event_dates->getNextObject()) {
-		$actual_date = date("Y-m-d", $event_date->getNumericValue());
-																	                        		$events[] = array($event_title, $actual_date);
-		}
-	}
-	return $events;
+function fnMailAssignees_new_task(&$article, &$user, &$text, &$summary, &$minoredit, &$watchthis, &$sectionanchor, &$flags, &$revision)
+{
+    fnMailAssignees(&$article, $user,'New task:','has just been assigned to you');
+    return TRUE;
 }
 
+function fnMailAssignees_updated_task(&$article, &$user, &$text, &$summary, &$minoredit, &$watchthis, &$sectionanchor, &$flags, &$revision)
+{
+    fnMailAssignees(&$article, $user,'Task updated:','has just been updated');
+    return TRUE;
 
-function st_updateAssignees(&$article, &$user, &$text, &$summary, &$minoredit, &$watchthis, &$sectionanchor, &$flags, $revision) {
-
-
-$page_title = $article->getTitle();
-// $user_info = smwfGetStore()->getPropertyValues($page_title, ");
-
-$f = 'webmaster+tasks@creativecommons.org'; // from mail address
-$t = 'nathan@creativecommons.org'; // to mail address
-
-// standard mail message RFC2822
-$m = 'From: '.$f."\r\n".
-     'To: '.$t."\r\n".
-          'Subject: test'."\r\n".
-	       'Content-Type: text/plain'."\r\n\r\n".
-	            $page_title;
-
-		    $h = explode('@', $t); // get client hostname
-		    $c = SMTP::MXconnect($h[1]); // connect to SMTP server (direct) from MX hosts list
-		    $s = SMTP::Send($c, array($t), $m, $f); // send mail
-		    // print result
-		    if ($s) echo 'Sent !';
-		    else print_r($_RESULT);
-		    SMTP::Disconnect($c); // disconnect
-
-   return TRUE;
+###########
+/*
+//Here starts the test for mail reminders 
+    fnRemindAssignees('Reminder:', 'Do not forget');
+*/
+###########
 }
 
-// FIXME: Notice pageCountInfo in the private article whatever
+function fnMailAssignees(&$article, &$user, $pre_title, $message)
+{
+    $title = $article->getTitle();
+    $subject = "$pre_title $title";
+    $from = new MailAddress($user->getEmail(),$user->getName());
 
-// This whole function is terrible
-function wpc_is_article_a_chapter_page(&$article, &$text) {
-    $wpcSearchStrings = array('[[Category:Chapter', '{{Chapter');
-    foreach($wpcSearchStrings as $needle) {
-        if (strstr($text, $needle)) {
-            return true;
-        }
+    /*
+    the send() function of MediaWiki only send plain text
+    so we can't use the linker->makelinkObj() method to generate a html link.
+        $l = new Linker();
+        $link = $l->makeLinkObj($title);
+    */
+    $link = $title->getFullURL();
+
+    $results = st_get_assignees_to_notify($title);
+    while ($row = $results->getNext())
+    {
+        $task_assignees = $row[0];
     }
-    return false;
+
+    $user_mailer = new UserMailer();
+
+    while ($task_assignee = $task_assignees->getNextObject())
+    {
+        $assignee_username = $task_assignee->getTitle();
+        $assignee_user_name = explode(":",$assignee_username);
+        $assignee_name = $assignee_user_name[1];
+        $body = "Hello $assignee_name, \nThe task \"$title\" $message.\n\n$link";
+
+
+        $assignee = User::newFromName($assignee_name);
+        $assignee_mail = new MailAddress($assignee->getEmail(),$assignee_name);
+
+        $user_mailer->send( $assignee_mail, $from, $subject, $body );
+    }
+
+    return TRUE;
 }
 
-# When an article is saved, call the function 'fnWikiPostCommit'
-# $wgHooks['ArticleSaveComplete'][] = 'st_updateAssignees';
+function st_get_assignees_to_notify(&$tasktitle)
+{
+    //We use the Semantic Media Wiki Processor
+    global $smwgIP;
+    include_once($smwgIP . "/includes/SMW_QueryProcessor.php");
+
+    $task_assignees = array();
+
+    $query_string = "[[$tasktitle]][[assigned to::*]]";
+    $params = array();
+    $inline = true;
+    $format = 'auto';
+    $printlabel = "";        
+    $printouts[] = new SMWPrintRequest(SMW_PRINT_THIS, $printlabel);
+
+    $query  = SMWQueryProcessor::createQuery($query_string, $params, $inline, $format, $printouts);        
+    $results = smwfGetStore()->getQueryResult($query);
+
+    return $results;
+}
+
+/*
+##########################################
+//Here is for email reminders
+
+function fnRemindAssignees($pre_title, $message)
+{
+    $t = getdate();
+    $today = date('F d Y',$t[0]);
+
+    $results = st_get_tasks_to_remind($today);
+    while ($row = $results->getNext())
+    {
+        $task_assignees = $row[0];
+    }
+
+    $user_mailer = new UserMailer();
+
+    while ($task_assignee = $task_assignees->getNextObject())
+    {
+        $assignee_username = $task_assignee->getTitle();
+        $assignee_user_name = explode(":",$assignee_username);
+        $assignee_name = $assignee_user_name[1];
+
+        $assignee = User::newFromName($assignee_name);
+        $assignee_mail = new MailAddress($assignee->getEmail(),$assignee_name);
+        $body = "Hello $assignee_name, \nThe task \"$title\" $message.\n\n$link";
+        $user_mailer->send( $assignee_mail, $from, $subject, $body );
+    }
+
+    return TRUE;
+}
+
+
+function st_get_tasks_to_remind(&$today)
+{
+    //We use the Semantic Media Wiki Processor
+    global $smwgIP;
+    include_once($smwgIP . "/includes/SMW_QueryProcessor.php");
+
+    $task_assignees = array();
+
+    $query_string = "[[reminder at::+]][[Target date::> $today]][[Assigned to::*]][[Reminder at::*]][[Target date::*]]";
+    $params = array();
+    $inline = true;
+    $format = 'auto';
+    $printlabel = "";        
+    $printouts[] = new SMWPrintRequest(SMW_PRINT_THIS, $printlabel);
+
+    $query  = SMWQueryProcessor::createQuery($query_string, $params, $inline, $format, $printouts);        
+    $results = smwfGetStore()->getQueryResult($query);
+
+    return $results;
+}
+//end of email reminders
+*/
+##########################################
+
+
+$wgHooks['ArticleSaveComplete'][] = 'fnMailAssignees_updated_task';
+$wgHooks['ArticleInsertComplete'][] = 'fnMailAssignees_new_task';
 
 ?>
