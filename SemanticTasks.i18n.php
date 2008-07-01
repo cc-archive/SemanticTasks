22ad410d1ada982bf1af1df948b0e52ec0e7b5b0
Steren <steren@creativecommons.org>
2008-07-01 10:44:03 -0700

    french i18n

--- SemanticTasks.i18n.php
+++ SemanticTasks.i18n.php
@@ -6,13 +6,17 @@ $messages['en'] = array(
     'assignedtoyou_msg' => "Hello $1,\nThe task \"$2\" has just been assigned to you.\n\n",
     'updatedtoyou_msg' => "Hello $1,\nThe task \"$2\" has just been updated.\n\n",
     'reminder' => 'Reminder: ',
-    'reminder-message' => "Hello $1, \nJust to remind you that the task \"$2\" ends in $3.\n\n$4",
+    'reminder-message' => "Hello $1, \nJust to remind you that the task \"$2\" ends in $3 days.\n\n$4",
 );
 
 
 $messages['fr'] = array(
     'newtask' => 'Nouvelle tâche :',
     'taskupdated' => 'Tâche mise à jour :',
+    'assignedtoyou_msg' => "Bonjour $1,\nLa tâche \"$2\" vous a été assignée.\n\n",
+    'updatedtoyou_msg' => "Bonjour $1,\nLa tâche \"$2\" a été mise à jour.\n\n",
+    'reminder' => 'Rappel: ',
+    'reminder-message' => "Bonjour $1, \nN'oubliez pas que la tâche \"$2\" se termine dans $3 jours.\n\n$4",
 );
 
 ?>
