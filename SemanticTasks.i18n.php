1d583e80ff67fc2d082785282f761a87f82f11a0
Steren <steren@creativecommons.org>
2008-07-01 10:39:57 -0700

    i18n file

---
+++ SemanticTasks.i18n.php
@@ -0,0 +1,18 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+    'newtask' => 'New Task:',
+    'taskupdated' => 'Task Updated:',
+    'assignedtoyou_msg' => "Hello $1,\nThe task \"$2\" has just been assigned to you.\n\n",
+    'updatedtoyou_msg' => "Hello $1,\nThe task \"$2\" has just been updated.\n\n",
+    'reminder' => 'Reminder: ',
+    'reminder-message' => "Hello $1, \nJust to remind you that the task \"$2\" ends in $3.\n\n$4",
+);
+
+
+$messages['fr'] = array(
+    'newtask' => 'Nouvelle tâche :',
+    'taskupdated' => 'Tâche mise à jour :',
+);
+
+?>
